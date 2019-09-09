13cfe05f856c3e7f9b48b7a63e6432a8427fdaa8
Daniel Wilson <drac1223@hotmail.com>
2019-09-09 12:09:39 +0930

    Framework mostly complete

---
+++ App/app/src/main/java/com/example/ntbargainhunter/DisplayBargainPage.java
@@ -0,0 +1,13 @@
+package com.example.ntbargainhunter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayBargainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_bargain_page);
+    }
+}
---
+++ App/app/src/main/java/com/example/ntbargainhunter/UserBargainList.java
@@ -0,0 +1,13 @@
+package com.example.ntbargainhunter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserBargainList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_bargain_list);
+    }
+}
