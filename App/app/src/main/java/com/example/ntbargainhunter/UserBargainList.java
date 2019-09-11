0b805a6293e6b73602ccd204b4a9e69d82930f85
Daniel Wilson <drac1223@hotmail.com>
2019-09-11 16:59:34 +0930

    Basic framework complete

--- App/app/src/main/java/com/example/ntbargainhunter/UserBargainList.java
+++ App/app/src/main/java/com/example/ntbargainhunter/UserBargainList.java
@@ -1,7 +1,9 @@
 package com.example.ntbargainhunter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserBargainList extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class UserBargainList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_bargain_list);
+    }
+
+    public void goToSignInPage(View v) {
+        Intent i = new Intent(this, SignInPage.class);
+        startActivity(i);
     }
 }
