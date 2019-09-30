06f22b1e1a2deed878ed192d5852a9c247bc01d3
Augustine <augustine.t@icloud.com>
2019-09-30 11:03:49 +0930

    comit

--- App/app/src/main/java/com/example/ntbargainhunter/PostBargainPage.java
+++ App/app/src/main/java/com/example/ntbargainhunter/PostBargainPage.java
@@ -1,15 +1,12 @@
 package com.example.ntbargainhunter;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -19,36 +16,44 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ServerTimestamp;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
-
+import id.zelory.compressor.Compressor;
 public class PostBargainPage extends AppCompatActivity {
 
-//    private Toolbar newpostToolbar;
+    private Toolbar newPostToolbar;
+
     private ImageView newPostImage;
     private EditText newPostDesc;
+    private EditText newPostTitle;
     private Button newPostBtn;
-
-    private Uri postImageUri=null;
+    private Uri postImageUri = null;
     private ProgressBar newPostProgress;
     private StorageReference storageReference;
     private FirebaseFirestore firebaseFirestore;
-//    private FirebaseAuth firebaseAuth;
-
-//    private String current_user_id;
-
+    private FirebaseAuth firebaseAuth;
+    private String current_user_id;
+    private Bitmap compressedImageFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,153 +61,178 @@ public class PostBargainPage extends AppCompatActivity {
         setContentView(R.layout.activity_post_bargain_page);
 
         storageReference = FirebaseStorage.getInstance().getReference();
-        firebaseFirestore=firebaseFirestore.getInstance();
-//        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-//        current_user_id = firebaseAuth.getCurrentUser().getUid();
+        current_user_id = firebaseAuth.getCurrentUser().getUid();
 
-//        newpostToolbar = findViewById(R.id.new_post_toolbar);
-//        setSupportActionBar(newpostToolbar);
-//        getSupportActionBar().setTitle("Add New Post");
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        newPostToolbar = findViewById(R.id.new_post_toolbar);
+        setSupportActionBar(newPostToolbar);
+        getSupportActionBar().setTitle("Add New Post");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        newPostImage=findViewById(R.id.new_post_image);
-
-        newPostDesc=findViewById(R.id.new_post_desc);
-        newPostBtn=findViewById(R.id.post_btn);
-        newPostProgress=findViewById(R.id.new_post_progress);
+        newPostImage = findViewById(R.id.imageButton);
+        newPostDesc = findViewById(R.id.descriptionText);
+        newPostTitle = findViewById(R.id.titleText);
+        newPostBtn = findViewById(R.id.submitbtn);
+        newPostProgress = findViewById(R.id.new_post_progress);
 
         newPostImage.setOnClickListener(new View.OnClickListener() {
-
-
-
             @Override
             public void onClick(View v) {
 
-
                 CropImage.activity()
                         .setGuidelines(CropImageView.Guidelines.ON)
-                        .setMinCropResultSize(512,512)
+                        .setMinCropResultSize(512, 512)
                         .setAspectRatio(1, 1)
                         .start(PostBargainPage.this);
-//permission pic
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
 
-                    if(ContextCompat.checkSelfPermission(PostBargainPage.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            }
+        });
 
-                        Toast.makeText(PostBargainPage.this, "Permission Denied", Toast.LENGTH_LONG).show();
-                        ActivityCompat.requestPermissions(PostBargainPage.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        newPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                    } else {
+                final String desc = newPostDesc.getText().toString();
+                final String title = newPostTitle.getText().toString();
+                if(!TextUtils.isEmpty(desc) && !TextUtils.isEmpty(title) && postImageUri != null){
 
-                        Toast.makeText(PostBargainPage.this, "you have permission", Toast.LENGTH_LONG).show();
+                    newPostProgress.setVisibility(View.VISIBLE);
 
+                    final String randomName = UUID.randomUUID().toString();
+
+                    // PHOTO UPLOAD
+                    File newImageFile = new File(postImageUri.getPath());
+                    try {
+
+                        compressedImageFile = new Compressor(PostBargainPage.this)
+                                .setMaxHeight(720)
+                                .setMaxWidth(720)
+                                .setQuality(50)
+                                .compressToBitmap(newImageFile);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
+
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    byte[] imageData = baos.toByteArray();
+
+                    // PHOTO UPLOAD
+
+                    UploadTask filePath = storageReference.child("bargain_images").child(randomName + ".jpg").putBytes(imageData);
+                    filePath.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull final Task<UploadTask.TaskSnapshot> task) {
+
+                            final String downloadUri = task.getResult().getDownloadUrl().toString();
+
+                            if(task.isSuccessful()){
+
+                                File newThumbFile = new File(postImageUri.getPath());
+                                try {
+
+                                    compressedImageFile = new Compressor(PostBargainPage.this)
+                                            .setMaxHeight(100)
+                                            .setMaxWidth(100)
+                                            .setQuality(1)
+                                            .compressToBitmap(newThumbFile);
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                                byte[] thumbData = baos.toByteArray();
+
+                                UploadTask uploadTask = storageReference.child("bargain_images/thumbs")
+                                        .child(randomName + ".jpg").putBytes(thumbData);
+
+                                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                                        String downloadthumbUri = taskSnapshot.getDownloadUrl().toString();
+
+                                        Map<String, Object> postMap = new HashMap<>();
+                                        postMap.put("image_url", downloadUri);
+                                        postMap.put("image_thumb", downloadthumbUri);
+                                        postMap.put("desc", desc);
+                                        postMap.put("title", title);
+                                        postMap.put("user_id", current_user_id);
+                                        postMap.put("timestamp", FieldValue.serverTimestamp());
+
+                                        firebaseFirestore.collection("Posts").add(postMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentReference> task) {
+
+                                                if(task.isSuccessful()){
+
+                                                    Toast.makeText(PostBargainPage.this, "Bargain was added", Toast.LENGTH_LONG).show();
+                                                    Intent mainIntent = new Intent(PostBargainPage.this, HomePage.class);
+                                                    startActivity(mainIntent);
+                                                    finish();
+
+                                                } else {
+
+
+                                                }
+
+                                                newPostProgress.setVisibility(View.INVISIBLE);
+
+                                            }
+                                        });
+
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+
+                                        //Error handling
+
+                                    }
+                                });
+
+
+                            } else {
+
+                                newPostProgress.setVisibility(View.INVISIBLE);
+
+                            }
+
+                        }
+                    });
+
 
                 }
 
             }
         });
-        newPostBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                final String desc = newPostDesc.getText().toString();
-                if (!TextUtils.isEmpty(desc) && postImageUri != null) {
-
-                    newPostProgress.setVisibility(View.VISIBLE);
-
-                    String randomName = FieldValue.serverTimestamp().toString();
-
-                    final StorageReference filepath= storageReference.child("post_image").child(randomName + ".jpg");
-                    filepath.putFile(postImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-
-                        @Override
-                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-
-                            if (task.isSuccessful()){
-
-                                filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                    @Override
-                                    public void onSuccess(Uri uri) {
-                                        final String downloadUri=uri.toString();
-                                        Map<String, Object> postMap = new HashMap<>();
-                                        postMap.put("image_url", downloadUri);
-//                                      postMap.put("image_thumb", downloadthumbUri);
-                                        postMap.put("desc", desc);
-//                                postMap.put("user_id", current_user_id);
-                                        postMap.put("timestamp", FieldValue.serverTimestamp());
-                                    }
-                                });
-
-//                                Task<Uri>result=task.getResult().getMetadata().getReference().getDownloadUrl();
-//                                String downloadUri= task.getResult().getUploadSessionUri().toString();
 
 
-
-
-
-                                firebaseFirestore.collection("posts").add(postImageUri).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<DocumentReference> task) {
-
-                                        if(task.isSuccessful()){
-
-                                            Toast.makeText(PostBargainPage.this, "Post was added", Toast.LENGTH_LONG).show();
-                                            Intent mainIntent = new Intent(PostBargainPage.this, HomePage.class);
-                                            startActivity(mainIntent);
-                                            finish();
-
-                                        }else{
-                                          System.out.println("Action could not be completed this time.");
-
-                                        }
-
-                                        newPostProgress.setVisibility(View.INVISIBLE);
-
-
-                                    }
-                                });
-
-                            }else{
-
-                                newPostProgress.setVisibility(View.INVISIBLE);
-
-
-                            }
-
-
-
-                        }
-
-
-
-
-                    });
-
-
-                }
-            }
-                });
-            }
-
-
-
-
+    }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
-            CropImage.ActivityResult result=CropImage.getActivityResult(data);
-            if(resultCode==RESULT_OK){
-               postImageUri=result.getUri();
-               newPostImage.setImageURI(postImageUri);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
 
-            }else if (resultCode==CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                postImageUri = result.getUri();
+                newPostImage.setImageURI(postImageUri);
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+
                 Exception error = result.getError();
+
             }
         }
-    }
+
     }
 
+}
