3e74bb375ae39b4a7840b7d68210e21d7970c368
Surendra Panday <panday.sp@gmail.com>
2019-10-12 11:44:15 +0930

    --unit tests refactored --fix bugs --moved files and ommitted redundant code

--- App/app/src/androidTest/java/com/example/ntbargainhunter/MockitoTest.java
+++ App/app/src/androidTest/java/com/example/ntbargainhunter/MockitoTest.java
@@ -1,20 +1,19 @@
-
 package com.example.ntbargainhunter;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-import com.google.common.truth.Truth.assertThat;
-import org.junit.Test;
 import android.app.Application;
 import android.content.Context;
+import android.support.test.runner.AndroidJUnit4;
 import android.support.test.runner.AndroidJUnitRunner;
 
-public class MockTestRunner extends AndroidJUnitRunner {
+import com.example.ntbargainhunter.HomeFragment;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MockitoTest extends AndroidJUnitRunner {
   @Override
   public Application newApplication(ClassLoader cl, String className, Context context)
       throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-    return super.newApplication(cl, MyMockApplication.class.getName(), context);
+    return super.newApplication(cl, HomeFragment.class.getName(), context);
   }
 }
